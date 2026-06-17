af3f05c345f90c8142a380c791653beb1fdb116e
jose luis <mamanijoss635@gmail.com>
2026-06-17 11:49:27 -0500

    ultima actualizacion

--- src/data/productos.js
+++ src/data/productos.js
@@ -1,9 +1,27 @@
 // src/data/productos.js
 export const productos = [
-  { id: '1', nombre: 'Bufanda de Lana Alpaca', categoria: 'Ropa', precio: 'S/ 45.00', descripcion: 'Hermosa bufanda tejida a mano, ideal para el invierno.', imagen: 'https://via.placeholder.com/150' },
-  { id: '2', nombre: 'Chaleco Pastel', categoria: 'Ropa', precio: 'S/ 80.00', descripcion: 'Chaleco en tonos pastel hecho a crochet.', imagen: 'https://via.placeholder.com/150' },
-  { id: '3', nombre: 'Amigurumi Osito', categoria: 'Peluches', precio: 'S/ 30.00', descripcion: 'Osito tejido con hilo de algodón hipoalergénico.', imagen: 'https://via.placeholder.com/150' },
-  { id: '4', nombre: 'Llavero de Cactus', categoria: 'Peluches', precio: 'S/ 12.00', descripcion: 'Mini cactus tierno para colgar en la mochila.', imagen: 'https://via.placeholder.com/150' },
-  { id: '5', nombre: 'Bolso Andino', categoria: 'Accesorios', precio: 'S/ 60.00', descripcion: 'Bolso resistente con diseños tradicionales.', imagen: 'https://via.placeholder.com/150' }
+  { 
+    id: '1', 
+    nombre: 'el loro', 
+    categoria: 'tejidos', 
+    precio: 'S/ 45.00', 
+    descripcion: 'hermoso loro ideal para poder regalar a tu crush y si no tienes lo regalas a tu amigo.', 
+    imagen: '/imagenes/el_loro.png' // Reemplaza por el nombre real de tu archivo
+  },
+  { 
+    id: '2', 
+    nombre: 'la gata', 
+    categoria: 'tejidos', 
+    precio: 'S/ 80.00', 
+    descripcion: 'es ideal para poder regalar a una chica linda como ella.', 
+    imagen: '/imagenes/la_gata.png' 
+  },
+  { 
+    id: '3', 
+    nombre: 'muñeco_ingeniero.png', 
+    categoria: 'tejidos ', 
+    precio: 'S/ 30.00', 
+    descripcion: 'ideal para poder regalar a tu novio que estudia ingenieria de sistemas.', 
+    imagen: 'imagenes/muñeco_ingeniero.png' 
+  }
 ];
-
