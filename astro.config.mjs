3fdd67bcce8edc6b8ba1466105b6b0a6aab46008
jose luis <mamanijoss635@gmail.com>
2026-08-04 00:17:30 -0500

    Corregida ruta dinámica de productos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel'; // 1. Importamos el adaptador de Vercel
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server', // 2. Activamos el modo SSR (Server-Side Rendering)
-  adapter: vercel(), // 3. Añadimos el adaptador de Vercel
+  output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
