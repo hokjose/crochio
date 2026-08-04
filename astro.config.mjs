39b437f420c9f7eacc7a14033597fe7e5c3bcce6
jose luis <mamanijoss635@gmail.com>
2026-08-04 00:15:01 -0500

    Configurar SSR y adaptador de Vercel en Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel'; // 1. Importamos el adaptador de Vercel
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // 2. Activamos el modo SSR (Server-Side Rendering)
+  adapter: vercel(), // 3. Añadimos el adaptador de Vercel
   vite: {
     plugins: [tailwindcss()]
   }
